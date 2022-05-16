654b97525dd7441e3aace98cc8e1fa199932946d
Aleksey Bykhun <theisease@gmail.com>
2022-05-16 13:45:09 +0800

    try new rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,62 +1,98 @@
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
-import autoprefixer from 'autoprefixer';
 import postcss from 'rollup-plugin-postcss';
+import filesize from 'rollup-plugin-filesize';
+import autoprefixer from 'autoprefixer';
+
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import json from '@rollup/plugin-json';
+import typescript from 'rollup-plugin-typescript2';
 
-// this override is needed because Module format cjs does not support top-level await
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const packageJson = require('./package.json');
 
-const globals = {
-    ...packageJson.devDependencies,
+import pkg from './package.json';
+
+const INPUT_FILE_PATH = 'src/package/index.tsx';
+const OUTPUT_NAME = 'Example';
+
+const GLOBALS = {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    'prop-types': 'PropTypes',
+
+    ...pkg.devDependencies,
 };
 
-const config = {
-    input: 'src/package/index.tsx',
+const PLUGINS = [
+    postcss({
+        extract: true,
+        plugins: [
+            autoprefixer,
+        ],
+    }),
+
+    peerDepsExternal(),
+    typescript({
+        useTsconfigDeclarationDir: true,
+        tsconfigOverride: {
+            include: ["src/package"],
+            exclude: ['**/*.stories.*'],
+        },
+    }),
+    json(),
+
+    babel({
+        babelHelpers: 'runtime',
+        exclude: 'node_modules/**',
+    }),
+    resolve({
+        browser: true,
+        resolveOnly: [
+            /^(?!react$)/,
+            /^(?!react-dom$)/,
+            /^(?!prop-types)/,
+        ],
+    }),
+    commonjs(),
+    filesize(),
+];
+
+const EXTERNAL = [
+    'react',
+    'react-dom',
+    'prop-types',
+];
+
+// https://github.com/rollup/plugins/tree/master/packages/babel#babelhelpers
+const CJS_AND_ES_EXTERNALS = EXTERNAL.concat(/@babel\/runtime/);
+
+const OUTPUT_DATA = [
+    // {
+    //     file: pkg.browser,
+    //     format: 'umd',
+    // },
+    {
+        file: pkg.main,
+        format: 'cjs',
+    },
+    {
+        file: pkg.module,
+        format: 'es',
+    },
+];
+
+const config = OUTPUT_DATA.map(({ file, format }) => ({
+    input: INPUT_FILE_PATH,
     inlineDynamicImports: true,
-    output: [
-        {
-            file: packageJson.main,
-            format: 'cjs', // commonJS
-            sourcemap: true,
-        },
-        {
-            file: packageJson.module,
-            format: 'esm', // ES Modules
-            sourcemap: true,
-        },
-    ],
-    plugins: [
-        peerDepsExternal(),
-        resolve(),
-        commonjs(),
-        typescript({
-            useTsconfigDeclarationDir: true,
-            tsconfigOverride: {
-                include: ["src/package"],
-                exclude: ['**/*.stories.*'],
-            },
-        }),
-        postcss({
-            plugins: [autoprefixer()],
-            sourceMap: true,
-            extract: true
-        }),
-        json(),
-        commonjs({
-            exclude: 'node_modules',
-            ignoreGlobal: true,
-        }),
-    ],
-    external: Object.keys(globals),
-};
+    output: {
+        file,
+        format,
+        name: OUTPUT_NAME,
+        globals: GLOBALS,
+        // sourcemap: true,
+    },
+    external: ['cjs', 'es'].includes(format) ? CJS_AND_ES_EXTERNALS : EXTERNAL,
+    plugins: PLUGINS,
+}));
 
 export default config;
-
-// Other useful plugins you might want to add are:
-// @rollup/plugin-images - import image files into your components
-// @rollup/plugin-json - import JSON files into your components
-// rollup-plugin-terser - minify the Rollup bundle
